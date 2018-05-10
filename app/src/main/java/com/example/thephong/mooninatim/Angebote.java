26a62005d35eba6e616b646d3623b327c24e4748
TPN <thephongnguyen@hotmail.de>
2018-05-10 18:16:32 +0200

    Neue Activity hinzugefügt "Angebote"; Buttons erstellt um auf das neue Activity zu switchen und zurück

--- app/src/main/java/com/example/thephong/mooninatim/Angebote.java
+++ app/src/main/java/com/example/thephong/mooninatim/Angebote.java
@@ -1,32 +1,31 @@
 package com.example.thephong.mooninatim;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-    private Button nxtbutton;
-
-
+public class Angebote extends AppCompatActivity {
+    private Button prevButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_angebote);
 
-        nxtbutton = findViewById(R.id.button);
-        nxtbutton.setOnClickListener(new View.OnClickListener() {
+        prevButton = findViewById(R.id.prvbtn);
+        prevButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openAngebote();
+                openStartBildschirm();
             }
         });
     }
 
-    public void openAngebote(){
-        Intent intent = new Intent(this, Angebote.class);
+    public void openStartBildschirm(){
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 }
